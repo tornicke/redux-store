85a733c17f2d92d7c0300dc8816f0fcd61e41dc6
tornicke <torniqe@gmail.com>
2022-11-13 16:24:44 +0100

    Adding store.js

---
+++ client/src/utils/store.js
@@ -0,0 +1,13 @@
+// Creates a Redux store that holds the state of the app. Only one store should exist.
+import { createStore } from "redux";
+
+// Importing the reducer file that is mostly unchanged
+import reducers from "./reducers";
+
+export default createStore(reducers, {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: "",
+});
